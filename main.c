67a86ac88b7fa11e78ee056d42a85a5a54655abb
Wald <kerwald.s@gmail.com>
2025-09-28 12:47:44 -0300

    adicionar verificação de primos com uso de threads

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ typedef struct {
     int* arg;
     int inicial;
     int final;
+    int nPrimos;
 } Info;
 
 int main(){
@@ -28,26 +29,58 @@ int main(){
         numThreads += 1;
     }
 
-    int j = quantidadePorThreads;
     Info info[numThreads];
+    pthread_t thread_id[numThreads];
 
     for( int i=0; i<numThreads; i++ ){
-        j = j*i; // indice inicial
-        info[i].inicial = j;
-        info[i].final = j + quantidadePorThreads - 1;
+        if( i==0 ){
+            info[i].inicial = 0;
+        }else{
+            info[i].inicial = quantidadePorThreads*i;
+        }
+        info[i].final = info[i].inicial + quantidadePorThreads - 1;
         info[i].arg = arg;
+        info[i].nPrimos = nPrimos;
 
-        pthread_t thread_id;
-        pthread_create (&thread_id, NULL, VerificaPrimo, (void*)&info[i] );
+        pthread_create ( &thread_id[i], NULL, VerificaPrimo, (void*)&info[i] );
     }
-    pthread_exit(NULL);
+
+    for( int i=0; i<numThreads; i++ ){
+        pthread_join( thread_id[i], NULL );
+    }
+
+    for( int i=0; i< nPrimos; i++){
+        if( arg[i] == 1 ){
+            printf( " %d ", i );
+        }
+    }
 
 
 }
 
 void *VerificaPrimo( void *argumento ){
 
+    Info info = *(Info*)argumento;
+    int ctrl;
 
+    if( info.final > info.nPrimos ){
+        info.final = info.nPrimos - 1;
+    }
+    
+    for( int i = info.inicial; i <= info.final; i++ ) {
+        ctrl = -1;
+        for( int j = 2; j<i; j++ ){
+            if( i%j == 0 ){
+                ctrl = 0;
+            }
+        }
+        if( ctrl == 0 && ( i != 0 || i != 1 ) ){
+            info.arg[i] = 0;
+        } else{
+            info.arg[i] = 1;
+        }
+    
+    }
 
 }
  
